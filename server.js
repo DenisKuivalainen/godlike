8e562389182229f6abd18bb231cb5df8a17936e7
AA0869 <aa0869@student.jamk.fi>
2020-08-17 14:12:58 +0300

    verify geoplugin

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080;
 
-// require('heroku-self-ping').default("https://mariadikki.herokuapp.com/");
- 
 const app = express();
  
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/listener', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'gen.html'));
+});
  
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
